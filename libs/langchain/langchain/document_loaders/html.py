8fad21b66bab6c1bc2fdc0ab8b192472fd1025ab
PresidioVantage <github@presidiovantage.com>
2023-11-04 16:46:42 -0700

    converted to proper Loader, attempting to test now

--- libs/langchain/langchain/document_loaders/html.py
+++ libs/langchain/langchain/document_loaders/html.py
@@ -1,7 +1,9 @@
 from typing import List
 
-from langchain.document_loaders.unstructured import UnstructuredFileLoader
+from langchain.document_loaders.base import BaseLoader
 
+from langchain.document_loaders.unstructured import UnstructuredFileLoader
+from langchain.document_loaders.parsers.html.html_chunker import HtmlChunker
 
 class UnstructuredHTMLLoader(UnstructuredFileLoader):
     """Load `HTML` files using `Unstructured`.
@@ -32,39 +34,40 @@ class UnstructuredHTMLLoader(UnstructuredFileLoader):
 
         return partition_html(filename=self.file_path, **self.unstructured_kwargs)
 
-class HTMLHeaderTextSplitter:
+class HTMLHeaderTextSplitter(BaseLoader):
     """
     Splitting HTML files based on specified headers.
     Requires lxml package.
     """
     
-    _DEFAULT_HEADER_MAPPING: dict[str, str] = [
-        ("h1", "article_main_heading_h1"),
+    _DEFAULT_HEADER_MAPPING: Dict[str, str] = {
+        "h1": "article_main_heading_h1",
         
-        ("h2", "article_subsection_heading_h2"),
-        ("h3", "article_subsection_heading_h3"),
-        ("h4", "article_subsection_heading_h4"),
-        ("h5", "article_subsection_heading_h5"),
-        ("h6", "article_subsection_heading_h6"),
+        "h2": "article_subsection_heading_h2",
+        "h3": "article_subsection_heading_h3",
+        "h4": "article_subsection_heading_h4",
+        "h5": "article_subsection_heading_h5",
+        "h6": "article_subsection_heading_h6",
         
-        ("D2 h1", "sub_article_subsection_heading_h07"),
-        ("D2 h2", "sub_article_subsection_heading_h08"),
-        ("D2 h3", "sub_article_subsection_heading_h09"),
-        ("D2 h4", "sub_article_subsection_heading_h10"),
-        ("D2 h5", "sub_article_subsection_heading_h11"),
-        ("D2 h6", "sub_article_subsection_heading_h12"),
+        "D2 h1": "sub_article_subsection_heading_h07",
+        "D2 h2": "sub_article_subsection_heading_h08",
+        "D2 h3": "sub_article_subsection_heading_h09",
+        "D2 h4": "sub_article_subsection_heading_h10",
+        "D2 h5": "sub_article_subsection_heading_h11",
+        "D2 h6": "sub_article_subsection_heading_h12",
         
-        ("D3 h1", "sub_sub_article_subsection_heading_h13"),
-        ("D3 h2", "sub_sub_article_subsection_heading_h14"),
-        ("D3 h3", "sub_sub_article_subsection_heading_h15"),
-        ("D3 h4", "sub_sub_article_subsection_heading_h16"),
-        ("D3 h5", "sub_sub_article_subsection_heading_h17"),
-        ("D3 h6", "sub_sub_article_subsection_heading_h18"),
-    ]
+        "D3 h1": "sub_sub_article_subsection_heading_h13",
+        "D3 h2": "sub_sub_article_subsection_heading_h14",
+        "D3 h3": "sub_sub_article_subsection_heading_h15",
+        "D3 h4": "sub_sub_article_subsection_heading_h16",
+        "D3 h5": "sub_sub_article_subsection_heading_h17",
+        "D3 h6": "sub_sub_article_subsection_heading_h18",
+    }
     
     def __init__(
         self,
-        header_mapping: Dict[str, Any] = dict(_DEFAULT_HEADER_MAPPING),
+        sources: Iterable[any],
+        header_mapping: Dict[str, Any] = None,
         return_each_element: bool = False,
     ):
         """Create a new HTMLHeaderTextSplitter.
@@ -73,38 +76,27 @@ class HTMLHeaderTextSplitter:
             header_mapping: dict of headers we want to track mapped to
                 (arbitrary) keys for metadata. Allowed header values: h1, h2, h3, h4,
                 h5, h6 e.g. [("h1", "Header 1"), ("h2", "Header 2)].
+                If 'None' (default), _DEFAULT_HEADER_MAPPING is used.
             return_each_element: Return each element w/ associated headers.
+                'False' to combine all adjacent chunks with identical metadata.
         """
-
-        if header_mapping is None:
-            header_mapping = self.DEFAULT_HEADER_MAPPING
         
-        self.header_mapping: dict[str, str] = header_mapping
-        self.header_capture: Collection[str] = {x[-2:] for x in header_mapping if x[-2:] in ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']}
+        if not sources:
+            raise Exception(f"HTMLHeaderTextSplitter(sources={sources})")
+        
+        self.sources: Iterable[any] = sources
+        
+        self.header_mapping: dict[str, str] = header_mapping if header_mapping else HTMLHeaderTextSplitter._DEFAULT_HEADER_MAPPING.copy()
+        self.header_capture: set[str] = {x[-2:] for x in header_mapping if x[-2:] in ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']}
         # Output element-by-element or aggregated into chunks w/ common headers
         self.return_each_element: bool = return_each_element
         
-        from langchain.document_loaders.parsers.html.html_chunker import HtmlChunker
         self.chunker: HtmlChunker = HtmlChunker(self.header_capture)
     
-    def split_text(self, text: str) -> List[Document]:
-        """Split HTML text string
-        
-        Args:
-            text: HTML text
-        """
-        
-        return self.split_text_from_source(StringIO(text))
-
-    def split_text_from_source(self, source: any) -> list[Document]:
-        """Split HTML file
-        
-        Args:
-            source: either: string (URL or file) or readable IO object (file/connection)
-        """
-        
-        return list(self._docsFromChunks(self.chunker.parseQueue(source, False)))
-    def split_text_from_sources(self, sources: Iterable[any]) -> Generator[Document]:
+    def load(self) -> List[Document]:
+        return list(self.lazy_load())
+    
+    def lazy_load(self) -> Iterator[Document]:
         """Split HTML file
         
         Args:
@@ -113,6 +105,20 @@ class HTMLHeaderTextSplitter:
         
         for source in sources:
             yield from self._docsFromChunks(self.chunker.parseQueue(source, False))
+    
+    @staticmethod
+    def split_text(
+        text: str,
+        header_mapping: Dict[str, Any] = None,
+        return_each_element: bool = False,
+    ) -> List[Document]:
+        """Split HTML text string
+        
+        Args:
+            text: HTML text
+        """
+        
+        return HTMLHeaderSplitter([StringIO(text)], header_mapping, return_each_element).load()
     
     # Helper Functions
     
@@ -124,9 +130,7 @@ class HTMLHeaderTextSplitter:
             page_content=chunk["text"],
             metadata={self.header_mapping.get(key, key): val for key, val in chunk["meta"].items()}
         )
-    def _aggregate_chunks_by_metadata(
-        self, chunks: Collection[dict[str, any]]
-    ) -> Generator[dict[str, any]]:
+    def _aggregate_chunks_by_metadata(self, chunks: Collection[dict[str, any]] ) -> Generator[dict[str, any]]:
         """Combine adjacent chunks with identical metadata.
         Should only be called with a single document's chunks.
         
